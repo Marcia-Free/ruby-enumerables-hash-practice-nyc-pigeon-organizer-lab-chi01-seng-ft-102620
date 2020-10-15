95ec96d96c283c8d39b65bcc9322dcc26bdfcd11
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-15 21:26:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  
+  data.each_with_object({}) do |(key, value), f_array|
+    f_array[key] = value
+    
+    value.each do |inner_key, names|
+      names.each do |name|
+        
+        if !f_array[name]
+          f_array[name] = {}
+        end
+        
+        if !f_array[name][key]
+          f_array[name][key] = []
+       # binding.pry
+        end
+
+    end
+    f_array
+   # binding.pry
+  end
+  
 end
