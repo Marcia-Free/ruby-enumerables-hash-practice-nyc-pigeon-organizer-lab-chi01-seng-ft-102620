a65489a9ae34a18615ae82574cac6a23171d636c
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-15 21:42:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
 
  name_hash = data.each_with_object({}) do |(key, value), f_array|
-    f_array[key] = value
-    
+
     value.each do |inner_key, names|
       names.each do |name|
         
